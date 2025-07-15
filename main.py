0db162421128c8dd62eb07b7e6356b08c5762dcf
athiyan99 <athiyankumar99@gmail.com>
2025-07-15 21:29:17 +0530

    basics

--- main.py
+++
@@ -1,7 +0,0 @@
-
-print("hello")
-
-num1=20
-num2=30
-print(num1+num2)
-print(num2-num1)
---
+++ test.py
@@ -0,0 +1,21 @@
+print("hello,madhurima")
+if 5>4:
+    print("true")
+x=5
+y=8
+print(x+5)
+print(x*y)
+print(type(x))
+fruits=["orrange","apple","banana"]
+a,b,c=fruits
+print(a)
+print(b)
+print(c)
+x="My"
+y="name is" 
+z="rahul"
+print(x,y,z)
+x="His"
+def myfunc():
+    print(x  + "pen")
+myfunc()    
